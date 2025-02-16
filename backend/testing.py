91cdfabeeb330a46beb079e67f0f7c122055b18d
ProcrastinateGuy <ching91202@gmail.com>
2025-02-16 04:21:55 -0800

    1. fixed a logic error in document.py: line 20, the efile_path should be passed in as an argument.

--- backend/testing.py
+++ backend/testing.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 import document
 import hashlib
 
@@ -19,6 +20,27 @@ class TestMyFunction(unittest.TestCase):
 
     def test_decrypt(self):
         self.assertEqual(self.inst.decrypt(), "Hi")
+
+    async def my_async_function():
+        file_path = os.path.join(UPLOAD_DIRECTORY, file.filename)
+        efile_path = os.path.join(UPLOAD_DIRECTORY, "Encrypted.txt")
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+            file.file.close() #close the clone file
+
+        tmp = document.Encryption(file_path)
+        f = open(efile_path, "w")
+        f.write(tmp.encrypt()) # will return an encrypted string of the cloned file
+        f.close()
+
+
+        return {"filename": file.filename, "message": "File uploaded successfully!", "path": file_path}
+
+
+    @pytest.mark.asyncio
+    async def test_my_async_function():
+        result = await my_async_function()
+        assert result == "result"
 
 if __name__ == '__main__':
     unittest.main()
