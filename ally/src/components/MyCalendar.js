bcbaa4305aa9c6101fa47f1fb98cca1231c8af75
Tanvi <54735473+sgtanvi@users.noreply.github.com>
2025-02-16 04:45:58 -0800

    simple fix

--- ally/src/components/MyCalendar.js
+++ ally/src/components/MyCalendar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
+import axios from 'axios';
 import "react-calendar/dist/Calendar.css";
 import "./CalendarStyles.css"; // Optional custom styles
 
