4d0a9cb015f2741cc884fbeaeb2e5b85e95bc390
ProcrastinateGuy <ching91202@gmail.com>
2025-02-15 23:17:06 -0800

    set up a testing branch for Sean

--- backend/document.py
+++ backend/document.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, FastAPI, File, UploadFile
 from config import UPLOAD_DIRECTORY  #  config for everyone's files
 import shutil
 import os
+import hashlib
 
 UPLOAD_DIRECTORY = "uploads"
 
@@ -13,5 +14,29 @@ async def upload_file(file: UploadFile = File(...)):
     
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
+        file.file.close() #close the clone file
+
+    tmp = Encryption(file_path)
+    f = open("Encrypted.txt", "w")
+    f.write(tmp.encrypt()) # will return a encrypted string of the cloned file
+    f.close()
+
 
     return {"filename": file.filename, "message": "File uploaded successfully!", "path": file_path}
+
+class Encryption:
+    def __init__(self, file_Path):
+        self.file_path = file_Path
+
+    def encrypt(self):
+        sha256 = hashlib.sha256()
+        sha256.update(self.file_path.encode())
+        return sha256.hexdigest()
+
+    def decrypt(self):
+        try:
+            file = open(self.file_path, 'r')
+            return file
+        except FileNotFoundError:
+            print(f"Error: File '{file}' not found.")
+            return None
